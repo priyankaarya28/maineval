a0fd45c29c216dfe03e78d11e3ad6d282c367d6a
priyanka arya <priyanka.arya00001@gmail.com>
2025-03-01 17:16:49 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 
 const app = require("express")
+var cron = require('node-cron');
+const Redis = require("ioredis");
 
 app.use(express.json());
+const redis = new Redis();
+redis.set("mykey", "value","EX",120);
+
+cron.schedule('1 * * * *', () => {
+    console.log('running every minute 1, 2, 4 and 5');
+  });
+  reddis.get("mykey",(err,result)=>{
+    if(err){
+        console.log(err)
+    }else{
+       console.log(result);
+        
+    }
+  })
 
 ///test route
 
 app.post("/",(req,res)=>{
     res.send("this is test route")
 })
-app.use("/Admin", AdminRoute)
+app.use("/admin", AdminRoute)
 app.use("/user", UserRoute)
 
 app.listen(8080, ()=>{
